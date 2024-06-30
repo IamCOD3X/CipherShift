fddda582b214f22fff0ff3463549bbbbb0629030
Sourabh Panchal <sourabh.panchal28@gmail.com>
2024-06-30 13:18:25 +0530

    Adding GUI Base

--- CaesarLock.py
+++ CaesarLock.py
@@ -1,1 +1,22 @@
 # Program to encrypt and decrypt text using ceasor cipher algorithm.
+import tkinter as tk
+from tkinter.ttk import *
+
+# Main program to encrypt and decrypt text using ceasor cipher algorithm
+
+
+
+# GUI implementation
+root = tk.Tk()
+root.title("CaesarLock")
+root.geometry("1200x580")
+root.resizable(False, False)
+
+#icon
+icon = tk.PhotoImage(file = "./assets/icon.png")
+root.iconphoto(False, icon)
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+root.mainloop()
